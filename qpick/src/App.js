f5276bb1cfec0b801da1ec02c56d1b92a650b2b5
Annapirz <annapirz@MacBook-Air-Annapirz.local>
2024-09-05 11:05:42 +0300

    feat: all pages ready

--- qpick/src/App.js
+++ qpick/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import MainPage from './pages/mainpage';
+import MainPage from './pages/mainpage';
 import Storebasket from './pages/store-basket';
 // import NotFound from './pages/notFound';
 import 'normalize.css';
@@ -9,7 +9,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Storebasket/>
+      <MainPage/>
     </div>
   );
 }
